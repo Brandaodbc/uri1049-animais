8233705b415ca0eec963a1fba5eba3434bd83c52
Diego Brandao Cantao <diegodbcatco@gmail.com>
2022-02-13 20:14:29 -0300

    acrescentado estrutura de repetição while

--- Main.java
+++ Main.java
@@ -11,65 +11,71 @@ public class Main {
 		String animalClass;
 		String animalType;
 		String animalAlim;
+		
+		int opc = 1;
 
-		animalClass = sc.next();
-		animalType = sc.next();
-		animalAlim = sc.next();
-
-		switch (animalClass) {
-		case "vertebrado":
-			if ("ave".equalsIgnoreCase(animalType)) {
-				
-				if ("carnivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("aguia");
-				} else if ("onivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("pomba");
-				}
-				
-			} 
-			else if ("mamifero".equalsIgnoreCase(animalType)) {
-				
-				if ("onivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("homem");
-				}
-				else if ("herbivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("vaca");
-				}
-				
-			}
+		while (opc == 1) {
 			
-			break;
-			
-		case "invertebrado":
+			animalClass = sc.next();
+			animalType = sc.next();
+			animalAlim = sc.next();
 
-			if ("inseto".equalsIgnoreCase(animalType)) {
-				
-				if ("hematofago".equalsIgnoreCase(animalAlim)) {
-					System.out.println("pulga");
-				}
-				else if ("herbivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("lagarta");
-				}
-				
-			}
-			else if ("anelideo".equalsIgnoreCase(animalType)) {
-				
-				if ("hematofago".equalsIgnoreCase(animalAlim)) {
-					System.out.println("sanguessuga");
-				}
-				else if ("onivoro".equalsIgnoreCase(animalAlim)) {
-					System.out.println("minhoca");
-				}
-				
-			}
-			
-			break;
+			switch (animalClass) {
+			case "vertebrado":
+				if ("ave".equalsIgnoreCase(animalType)) {
 
-		default:
-			System.out.println("Tipo de animal inexistente");
-			break;
+					if ("carnivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("aguia");
+					} else if ("onivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("pomba");
+					}
+
+				} else if ("mamifero".equalsIgnoreCase(animalType)) {
+
+					if ("onivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("homem");
+					} else if ("herbivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("vaca");
+					}
+
+				}
+
+				break;
+
+			case "invertebrado":
+
+				if ("inseto".equalsIgnoreCase(animalType)) {
+
+					if ("hematofago".equalsIgnoreCase(animalAlim)) {
+						System.out.println("pulga");
+					} else if ("herbivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("lagarta");
+					}
+
+				} else if ("anelideo".equalsIgnoreCase(animalType)) {
+
+					if ("hematofago".equalsIgnoreCase(animalAlim)) {
+						System.out.println("sanguessuga");
+					} else if ("onivoro".equalsIgnoreCase(animalAlim)) {
+						System.out.println("minhoca");
+					}
+
+				}
+
+				break;
+
+			default:
+				System.out.println("Tipo de animal inexistente");
+				break;
+			}
+
+			System.out.print("Deseja realiza nova consulta? [1-Sim/0-Nao] ");
+			opc = sc.nextInt();
+
 		}
-
+		
+		System.out.println("Fim da execucao do programa");
+		
 		sc.close();
 	}
 
